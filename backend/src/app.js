ceebfa1800a7da28d528758b13e4a8e7807178c4
deepanshu0619 <dst0619@gmail.com>
2026-06-19 20:31:50 +0530

    Add backend and frontend source code

---
+++ backend/src/app.js
@@ -0,0 +1,44 @@
+import "dotenv/config";
+import express from "express";
+import { createServer } from "node:http";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
+
+const start = async () => {
+    const MONGO_URI = process.env.MONGO_URI;
+    if (!MONGO_URI) {
+        console.error("MONGO_URI is not defined in environment variables");
+        process.exit(1);
+    }
+    const connectionDb = await mongoose.connect(MONGO_URI)
+
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log(`LISTENIN ON PORT ${app.get("port")}`)
+    });
+
+
+
+}
+
+
+
+start();
